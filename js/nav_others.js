5256f9ea5a3fb370429ac76bb91994229e502f1d
jaggedmule14 <92223496+jaggedmule14@users.noreply.github.com>
2022-12-31 21:04:11 -0600

    Add files via upload

---
+++ js/nav_others.js
@@ -0,0 +1,35 @@
+
+(() => {
+	let TypingSpeed = 150;
+//	let NxtMsgDelay = 1000;
+	let CharacterPos = 0;
+	let MsgBuffer = "";
+	let MsgIndex = 0;
+	let delay;
+	let id = document.getElementById("typing-text4");
+	let messages =
+[
+	"Others"
+]
+
+	const StartTyping = () => {
+		if (CharacterPos != messages[MsgIndex].length) {
+			MsgBuffer += messages[MsgIndex].charAt(CharacterPos);
+			id.value = MsgBuffer+(CharacterPos != messages[MsgIndex].length-1 ? "_":"");
+			delay = TypingSpeed;
+			id.scrollTop = id.scrollHeight; 
+		} else {
+			delay = NxtMsgDelay;
+			MsgBuffer   = "";
+			CharacterPos = -1;
+			if (MsgIndex!= messages.length-1){
+				MsgIndex++;
+			}else {
+				MsgIndex = 0;
+			}
+		}
+		CharacterPos++;
+		setTimeout(StartTyping, delay);
+	}
+	StartTyping();
+})();
